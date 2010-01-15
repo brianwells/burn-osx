d3a90a2dd70381f340f14e2c7d4522e698e44f25
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-15 12:29:03 +0000

    Cleaned to only contain application related actions and outlets.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@119 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWApplication.h
+++ burn/Source/KWApplication.h
@@ -1,59 +1,22 @@
-/* appController */
+/* KWApplication */
 
 #import <Cocoa/Cocoa.h>
-#import <DiscRecording/DiscRecording.h>
-#import <KWEraser.h>
-#import <KWProgress.h>
 #import "KWPreferences.h"
 #import "KWRecorderInfo.h"
-#import "KWDiskInfo.h"
+#import "KWDiscInfo.h"
 #import "KWEjecter.h"
 #import "KWInspector.h"
-#import "KWConsole.h"
 
-@interface appController : NSObject
+@interface KWApplication : NSObject
 {
-    //Menu Items
-	//Burn Menu
-	IBOutlet id preferencesBurn;
-	//File Menu
-	IBOutlet id fileMenu;
-	IBOutlet id openFile;
-	IBOutlet id closeFile;
-	IBOutlet id saveAsFile;
-    IBOutlet id saveImageFile;
-	//Recorder Menu
-	IBOutlet id eraseRecorder;
-	IBOutlet id ejectRecorder;
-	IBOutlet id burnRecorder;
-	//Items Menu
-	IBOutlet id addItems;
-	IBOutlet id deleteItems;
-	IBOutlet id createFolderItems;
-	IBOutlet id mountImageItems;
-	IBOutlet id scanDisksItems;
-	//Window Menu
-	IBOutlet id windowMenu;
-	IBOutlet id minimizeWindow;
-	IBOutlet id zoomWindow;
-	IBOutlet id returnToDefaultSizeWindow;
-	IBOutlet id burnWindow;
-	IBOutlet id inspectorWindow;
-	IBOutlet id mediaBrowserWindow;
-	IBOutlet id recorderInfoWindow;
-	IBOutlet id diskInfoWindow;
-	IBOutlet id bringAllToFrontWindow;
-    //Help Menu
-	IBOutlet id itemHelp;
-    
     //Variables
 	KWPreferences *preferences;
 	KWRecorderInfo *recorderInfo;
-	KWDiskInfo *diskInfo;
+	KWDiscInfo *diskInfo;
 	KWEjecter *ejecter;
 	KWInspector *inspector;
-	KWConsole *console;
 	
+	//Inspector variables
 	id currentObject;
 	NSString *currentType;
 }
@@ -61,24 +24,11 @@
 //Menu Actions
 //Burn Menu
 - (IBAction)preferencesBurn:(id)sender;
-//File Menu
-- (IBAction)openFile:(id)sender;
-//Recorder Menu
-- (IBAction)eraseRecorder:(id)sender;
-- (IBAction)ejectRecorder:(id)sender;
 //Window Menu
-- (IBAction)burnWindow:(id)sender;
 - (IBAction)inspectorWindow:(id)sender;
 - (IBAction)recorderInfoWindow:(id)sender;
 - (IBAction)diskInfoWindow:(id)sender;
 //Help Menu
-- (IBAction)itemHelp:(id)sender;
 - (IBAction)openBurnSite:(id)sender;
-
-//Notification Actions
-- (void)controlMenus:(NSNotification *)notif;
-- (void)closeWindow:(NSNotification *)notif;
-- (void)changeBurnStatus:(NSNotification *)notif;
-- (void)openPreferencesAndAddTheme:(NSNotification *)notif;
 
 @end
