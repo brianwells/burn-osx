d484b9cd6d630885a55deb06f210944def558b14
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-15 12:43:16 +0000

    * Removed code to run as window (instead of a sheet, won't happen any more) * Added error handling when it fails * Cleaned up code.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@122 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWEjecter.m
+++ burn/Source/KWEjecter.m
@@ -1,4 +1,5 @@
 #import "KWEjecter.h"
+#import "KWCommonMethods.h"
 
 @implementation KWEjecter
 
@@ -6,7 +7,7 @@
 {
 	if( self = [super init] )
 	{
-	[NSBundle loadNibNamed:@"KWEjecter" owner:self];
+		[NSBundle loadNibNamed:@"KWEjecter" owner:self];
 	}
 	
 	return self;
@@ -14,46 +15,57 @@
 
 - (void)dealloc
 {
-[super dealloc];
+	[super dealloc];
 }
+
+//////////////////
+// Main actions //
+//////////////////
+
+#pragma mark -
+#pragma mark •• Main actions
 
 - (void)startEjectSheetForWindow:(NSWindow *)atachWindow forDevice:(DRDevice *)device
 {
-[popupButton removeAllItems];
-
+	[popupButton removeAllItems];
+	
+	NSArray *devices = [DRDevice devices];
 	int i;
-	for (i=0;i< [[DRDevice devices] count];i++)
+	for (i=0;i< [devices count];i++)
 	{
-	[popupButton addItemWithTitle:[[[DRDevice devices] objectAtIndex:i] displayName]];
+		[popupButton addItemWithTitle:[[devices objectAtIndex:i] displayName]];
 	}
 	
-[popupButton selectItemWithTitle:[device displayName]];
+	[popupButton selectItemWithTitle:[device displayName]];
 
-	if (!atachWindow == nil)
-	{
 	[NSApp beginSheet:[self window] modalForWindow:atachWindow modalDelegate:self didEndSelector: @selector(sheetDidEnd:returnCode:contextInfo:) contextInfo:nil];
-	}
-	else
-	{
-	[[self window] makeKeyAndOrderFront:self];
-	}
 }
 
 - (void)sheetDidEnd:(NSWindow *)sheet returnCode:(int)returnCode contextInfo:(void *)contextInfo
 {
-[sheet orderOut:self];
+	[sheet orderOut:self];
 }
+
+///////////////////////
+// Interface actions //
+///////////////////////
+
+#pragma mark -
+#pragma mark •• Interface actions
 
 - (IBAction)cancelEject:(id)sender
 {
-[NSApp endSheet:[self window]];
+	[NSApp endSheet:[self window]];
 }
 
 - (IBAction)ejectDisk:(id)sender
 {
-[[[DRDevice devices] objectAtIndex:[popupButton indexOfSelectedItem]] ejectMedia];
+	if (![[[DRDevice devices] objectAtIndex:[popupButton indexOfSelectedItem]] ejectMedia])
+	{
+		[KWCommonMethods standardAlertWithMessageText:NSLocalizedString(@"Failed to eject", Localized) withInformationText:NSLocalizedString(@"Could not eject media from the drive", Localized)withParentWindow:nil];
+	}
 
-[NSApp endSheet:[self window]];
+	[NSApp endSheet:[self window]];
 }
 
 @end
