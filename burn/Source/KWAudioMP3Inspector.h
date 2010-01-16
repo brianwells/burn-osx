13a757df48c72a5bd550ce03cc4a17e105b993c2
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-16 13:28:28 +0000

    * Removed outlets. * Added an mapping variable to replace the removed outlets.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@150 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWAudioMP3Inspector.h
+++ burn/Source/KWAudioMP3Inspector.h
@@ -10,32 +10,26 @@
 	IBOutlet id iconView;
 	IBOutlet id tabView;
 	
-    IBOutlet id album;
-    IBOutlet id artist;
-    IBOutlet id composer;
-    IBOutlet id discNumber;
-    IBOutlet id discTotal;
-    IBOutlet id genre;
     IBOutlet id imageString;
     IBOutlet id imageView;
-    IBOutlet id myView;
-    IBOutlet id notes;
-    IBOutlet id title;
-    IBOutlet id trackNumber;
-    IBOutlet id trackTotal;
-    IBOutlet id year;
-	
-	id currentObjects;
+    
+	IBOutlet id myView;
+
+	//Variables
 	TagAPI *Tag;
+	NSTableView *currentTableView;
+	NSArray *methodMappings;
+	int currentIndex;
 }
 - (void)updateView:(id)object;
-- (id)getObjectWithSelector:(SEL)selector fromObjects:(NSArray *)objects returnsInt:(BOOL)isInt;
+- (id)getObjectWithSelector:(SEL)selector fromObjects:(NSArray *)objects;
 - (void)setObjectWithSelector:(SEL)selector forObjects:(NSArray *)objects withObject:(id)object;
 - (IBAction)addImage:(id)sender;
 - (IBAction)nextImage:(id)sender;
 - (IBAction)optionsChanged:(id)sender;
 - (IBAction)previousImage:(id)sender;
 - (IBAction)removeImage:(id)sender;
-- (void)updateArtWorkAtIndex:(int)index;
+- (void)updateArtWork;
 - (id)myView;
+
 @end
