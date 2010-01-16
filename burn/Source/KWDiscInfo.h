ce1c63aacfb5a7df1bc68437d1ad2aea16238676
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-16 12:33:36 +0000

    * Changed 'KWDiskInfo' to 'KWDiscInfo'. * Removed internal call. * Cleaned up code.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@125 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWDiscInfo.h
+++ burn/Source/KWDiscInfo.h
@@ -1,22 +1,28 @@
-/* KWDiskInfo */
+/* KWDiscInfo */
 
 #import <Cocoa/Cocoa.h>
 #import <DiscRecording/DiscRecording.h>
 
-@interface KWDiskInfo : NSWindowController
+@interface KWDiscInfo : NSWindowController
 {
     IBOutlet id freeSpaceDisk;
     IBOutlet id kindDisk;
     IBOutlet id recorderPopup;
     IBOutlet id usedSpaceDisk;
     IBOutlet id writableDisk;
+	
+	NSDictionary *discTypes;
 }
+
+//Main actions
+- (void)startDiskPanelwithDevice:(DRDevice *)device;
+
 //Interface actions
 - (IBAction)recorderPopup:(id)sender;
 
-//Own actions
-- (void)startDiskPanelwithDevice:(DRDevice *)device;
+//Internal actions
 - (void)setDiskInfo:(DRDevice *)device;
 - (void)updateDiskInfo;
+- (void)saveFrame;
 
 @end
