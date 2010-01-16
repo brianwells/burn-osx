af35a595e46a5ba805cdac5041ec9207afb5b2f2
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-16 13:22:56 +0000

    * Removed outlets. * Added an mapping variable to replace the removed outlets.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@146 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWAudioInspector.h
+++ burn/Source/KWAudioInspector.h
@@ -4,32 +4,24 @@
 
 @interface KWAudioInspector : NSObject
 {
-	IBOutlet id title;
-	IBOutlet id performer;
-	IBOutlet id composer;
-	IBOutlet id songwriter;
-	IBOutlet id arranger;
-	IBOutlet id notes;
-	IBOutlet id privateUse;
-    IBOutlet id indexPoints;
-    IBOutlet id ISRCCDText;
-    IBOutlet id ISRCCheckBox;
-    IBOutlet id ISRCField;
-    IBOutlet id preEmphasis;
-    IBOutlet id preGap;
+	//Interface outlets
 	IBOutlet id invalid;
-	
 	IBOutlet id myView;
 	IBOutlet id iconView;
 	IBOutlet id nameField;
     IBOutlet id timeField;
-	id currentTableView;
+	
+	//Variables
+	NSTableView *currentTableView;
+	NSArray *tagMappings;
 }
+
 - (void)updateView:(id)object;
-- (id)getObjectForKey:(NSString *)key inObjects:(NSArray *)objects;
+- (id)getObjectForKey:(NSString *)key inObject:(id)object atIndexes:(NSArray *)indexes;
 - (IBAction)optionsChanged:(id)sender;
-- (IBAction)ISRCCheckBox:(id)sender;
 - (IBAction)ISRCChanged:(id)sender;
 - (BOOL)isValidISRC:(NSString*)isrc;
+- (NSString *)ISRCStringFromString:(NSString *)string;
 - (id)myView;
+
 @end
