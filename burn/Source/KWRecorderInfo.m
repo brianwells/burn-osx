190797db66d60f643a0c7b8b4fb72c05601b09f7
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-16 12:43:37 +0000

    * Cleaned up and rearranged code.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@128 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWRecorderInfo.m
+++ burn/Source/KWRecorderInfo.m
@@ -5,38 +5,92 @@
 
 - (id)init
 {
-self = [super init];
-
-[NSBundle loadNibNamed:@"KWRecorderInfo" owner:self];
-
-return self;
+	if( self = [super init] )
+	{
+		NSArray *objects = [NSArray arrayWithObjects:	@"CD-R",
+														@"CD-RW",
+														@"DVD-R",
+														@"DVD-RW",
+														@"DVD-RAM",
+														@"DVD+R",
+														@"DVD+R(DL)",
+														@"DVD+RW",
+		nil];
+		
+		NSArray *keys = [NSArray arrayWithObjects:	@"DRDeviceCanWriteCDRKey",
+													@"DRDeviceCanWriteCDRWKey",
+													@"DRDeviceCanWriteDVDRKey",
+													@"DRDeviceCanWriteDVDRWKey",
+													@"DRDeviceCanWriteDVDRAMKey",
+													@"DRDeviceCanWriteDVDPlusRKey",
+													@"DRDeviceCanWriteDVDPlusRDoubleLayerKey",
+													@"DRDeviceCanWriteDVDPlusRWKey",
+		nil];
+		
+		discTypes = [[NSDictionary alloc] initWithObjects:objects forKeys:keys];
+	
+		[NSBundle loadNibNamed:@"KWRecorderInfo" owner:self];
+	}
+	
+	return self;
 }
 
 - (void)dealloc
 {
-[[DRNotificationCenter currentRunLoopCenter] removeObserver:self name:DRDeviceStatusChangedNotification object:nil];
-[[NSNotificationCenter defaultCenter] removeObserver:self];
+	[[DRNotificationCenter currentRunLoopCenter] removeObserver:self name:DRDeviceStatusChangedNotification object:nil];
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
 
-[super dealloc];
+	[super dealloc];
 }
 
 - (void)awakeFromNib
 {
-[[DRNotificationCenter currentRunLoopCenter] addObserver:self selector:@selector(updateRecorderInfo) name:DRDeviceDisappearedNotification object:nil];
-[[DRNotificationCenter currentRunLoopCenter] addObserver:self selector:@selector(updateRecorderInfo) name:DRDeviceAppearedNotification object:nil];
+	NSWindow *myWindow = [self window];
+	DRNotificationCenter *currentCenter = [DRNotificationCenter currentRunLoopCenter];
 
-[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(saveFrame) name:NSWindowWillCloseNotification object:nil];
+	[currentCenter addObserver:self selector:@selector(updateRecorderInfo) name:DRDeviceDisappearedNotification object:nil];
+	[currentCenter addObserver:self selector:@selector(updateRecorderInfo) name:DRDeviceAppearedNotification object:nil];
 
-[[self window] setFrameUsingName:@"Recorder Info"];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(saveFrame) name:NSWindowWillCloseNotification object:nil];
+
+	[myWindow setFrameUsingName:@"Recorder Info"];
 
 	if ([[NSUserDefaults standardUserDefaults] boolForKey:@"KWFirstRun"] == YES)
-	[[self window] setFrameOrigin:NSMakePoint(500,[[NSScreen mainScreen] frame].size.height - 310)];
+		[myWindow setFrameOrigin:NSMakePoint(500,[[NSScreen mainScreen] frame].size.height - 310)];
 }
 
-- (void)saveFrame
+//////////////////
+// Main actions //
+//////////////////
+
+#pragma mark -
+#pragma mark •• Main actions
+
+- (void)startRecorderPanelwithDevice:(DRDevice *)device
 {
-[[self window] saveFrameUsingName:@"Recorder Info"];
-[[NSUserDefaults standardUserDefaults] synchronize];
+	NSWindow *myWindow = [self window];
+
+	if ([myWindow isVisible])
+	{
+		[myWindow orderOut:self];
+	}
+	else
+	{
+		[recorderPopup removeAllItems];
+
+		NSArray *devices = [DRDevice devices];
+		int i;
+		for (i=0;i< [devices count];i++)
+		{
+			[recorderPopup addItemWithTitle:[[devices objectAtIndex:i] displayName]];
+		}
+			
+		[recorderPopup selectItemWithTitle:[device displayName]];
+		
+		[self setRecorderInfo:device];
+		
+		[myWindow makeKeyAndOrderFront:self];
+	}
 }
 
 ///////////////////////
@@ -48,121 +102,97 @@ return self;
 
 - (IBAction)recorderPopup:(id)sender
 {
-NSArray *devices = [DRDevice devices];
-[self setRecorderInfo:[devices objectAtIndex:[recorderPopup indexOfSelectedItem]]];
+	NSArray *devices = [DRDevice devices];
+	[self setRecorderInfo:[devices objectAtIndex:[recorderPopup indexOfSelectedItem]]];
 }
 
-/////////////////
-// Own actions //
-/////////////////
+//////////////////////
+// Internal actions //
+//////////////////////
 
 #pragma mark -
-#pragma mark •• Own actions
-
-- (void)startRecorderPanelwithDevice:(DRDevice *)device
-{
-NSArray *devices = [DRDevice devices];
-
-[recorderPopup removeAllItems];
-		
-	int i;
-	for (i=0;i< [devices count];i++)
-	{
-	[recorderPopup addItemWithTitle:[[devices objectAtIndex:i] displayName]];
-	}
-			
-[recorderPopup selectItemWithTitle:[device displayName]];
-		
-[self setRecorderInfo:device];
-
-	if ([[self window] isVisible])
-	[[self window] orderOut:self];
-	else
-	[[self window] makeKeyAndOrderFront:self];
-}
+#pragma mark •• Internal actions
 
 - (void)setRecorderInfo:(DRDevice *)device
 {
-[recorderProduct setStringValue:[[device info] objectForKey:@"DRDeviceProductNameKey"]];
-[recorderVendor setStringValue:[[device info] objectForKey:@"DRDeviceVendorNameKey"]];
-[recorderConnection setStringValue:[[device info] objectForKey:@"DRDevicePhysicalInterconnectKey"]];
-[recorderCache setStringValue:[[[[device info] objectForKey:@"DRDeviceWriteBufferSizeKey"] stringValue] stringByAppendingString:@" KB"]];
+	NSDictionary *deviceInfo = [device info];
 
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanUnderrunProtectCDKey] stringValue] isEqualTo:@"1"])
-	[recorderBuffer setStringValue:NSLocalizedString(@"Yes",@"Localized")];
-	else
-	[recorderBuffer setStringValue:NSLocalizedString(@"No",@"Localized")];
+	[recorderProduct setStringValue:[deviceInfo objectForKey:@"DRDeviceProductNameKey"]];
+	[recorderVendor setStringValue:[deviceInfo objectForKey:@"DRDeviceVendorNameKey"]];
+	[recorderConnection setStringValue:[deviceInfo objectForKey:@"DRDevicePhysicalInterconnectKey"]];
+	[recorderCache setStringValue:[NSString localizedStringWithFormat:@"%.0f KB", [deviceInfo objectForKey:@"DRDeviceWriteBufferSizeKey"]]];
+
+	NSDictionary *writeCapabilities = [deviceInfo objectForKey:@"DRDeviceWriteCapabilitiesKey"];
+	BOOL cdUnderrunProtect = [[writeCapabilities objectForKey:DRDeviceCanUnderrunProtectCDKey] boolValue];
+	BOOL canWriteDVD = [[writeCapabilities objectForKey:DRDeviceCanWriteDVDKey] boolValue];
 	
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteDVDKey] stringValue] isEqualTo:@"1"])
+	if (cdUnderrunProtect && !canWriteDVD)
+		[recorderBuffer setStringValue:NSLocalizedString(@"Yes",nil)];
+	
+	if (canWriteDVD)
 	{
-		if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanUnderrunProtectDVDKey] stringValue] isEqualTo:@"1"])
-		{
-			if ([[recorderBuffer stringValue] isEqualTo:NSLocalizedString(@"Yes",@"Localized")])
-			[recorderBuffer setStringValue:[[[@"CD: " stringByAppendingString:NSLocalizedString(@"Yes",@"Localized")] stringByAppendingString:@" DVD: "] stringByAppendingString:NSLocalizedString(@"Yes",@"Localized")]];
-			else
-			[recorderBuffer setStringValue:[[[@"CD: " stringByAppendingString:NSLocalizedString(@"No",@"Localized")] stringByAppendingString:@" DVD: "] stringByAppendingString:NSLocalizedString(@"Yes",@"Localized")]];
-		}
+		BOOL dvdUnderrunProtect = [[writeCapabilities objectForKey:DRDeviceCanUnderrunProtectDVDKey] boolValue];
+		NSString *cdUnderrun;
+		NSString *dvdUnderrun;
+		
+		if (cdUnderrunProtect)
+			cdUnderrun = NSLocalizedString(@"Yes",nil);
 		else
-		{
-			if ([[recorderBuffer stringValue] isEqualTo:NSLocalizedString(@"Yes",@"Localized")])
-			[recorderBuffer setStringValue:[[[@"CD: " stringByAppendingString:NSLocalizedString(@"Yes",@"Localized")] stringByAppendingString:@" DVD: "] stringByAppendingString:NSLocalizedString(@"No",@"Localized")]];
-			else
-			[recorderBuffer setStringValue:[[[@"CD: " stringByAppendingString:NSLocalizedString(@"No",@"Localized")] stringByAppendingString:@" DVD: "] stringByAppendingString:NSLocalizedString(@"No",@"Localized")]];
-		}
+			cdUnderrun = NSLocalizedString(@"No",nil);
+			
+		if (dvdUnderrunProtect)
+			dvdUnderrun = NSLocalizedString(@"Yes",nil);
+		else
+			dvdUnderrun = NSLocalizedString(@"No",nil);
+			
+		[recorderBuffer setStringValue:[NSString stringWithFormat:@"CD: %@ DVD: %@", cdUnderrun, dvdUnderrun]];
 	}
 	
-NSString *writesOn = @"";
+	NSArray *typeKeys = [discTypes allKeys];
+	NSString *writesOn = @"";
+	NSString *space = @"";
 	
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteCDRKey] stringValue] isEqualTo:@"1"])
-	writesOn = [writesOn stringByAppendingString:@"CD-R "];
+	int i;
+	for (i=0;i< [typeKeys count];i++)
+	{
+	NSString *currentKey = [typeKeys objectAtIndex:i];
 	
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteCDRWKey] stringValue] isEqualTo:@"1"])
-	writesOn = [writesOn stringByAppendingString:@"CD-RW "];
-	
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteDVDRKey] stringValue] isEqualTo:@"1"])
-	writesOn = [writesOn stringByAppendingString:@"DVD-R "];
-	
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteDVDRWKey] stringValue] isEqualTo:@"1"])
-	writesOn = [writesOn stringByAppendingString:@"DVD-RW "];
-
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteDVDRAMKey] stringValue] isEqualTo:@"1"])
-	writesOn = [writesOn stringByAppendingString:@"DVD-RAM "];
-
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteDVDPlusRKey] stringValue] isEqualTo:@"1"])
-	writesOn = [writesOn stringByAppendingString:@"DVD+R "];
-	
-	if (![KWCommonMethods isPanther])
-		if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteDVDPlusRDoubleLayerKey] stringValue] isEqualTo:@"1"])
-		writesOn = [writesOn stringByAppendingString:@"DVD+R(DL) "];
-	
-	if ([[[[[device info] objectForKey:@"DRDeviceWriteCapabilitiesKey"] objectForKey:DRDeviceCanWriteDVDPlusRWKey] stringValue] isEqualTo:@"1"])
-	writesOn = [writesOn stringByAppendingString:@"DVD+RW"];
+		if ([[writeCapabilities objectForKey:currentKey] boolValue])
+		{
+			writesOn = [NSString stringWithFormat:@"%@%@%@", writesOn, space, [discTypes objectForKey:currentKey]];
+			space = @" ";
+		}
+	}
 	
 	[recorderWrites setStringValue:writesOn];
 }
 
 - (void)updateRecorderInfo
 {
-NSArray *devices = [DRDevice devices];
+	NSArray *devices = [DRDevice devices];
 	
-NSString *title = [[recorderPopup title] copy];
-BOOL deviceStillExsists = NO;
-[recorderPopup removeAllItems];
+	NSString *title = [[recorderPopup title] copy];
+
+	[recorderPopup removeAllItems];
 		
 	int i;
 	for (i=0;i< [devices count];i++)
 	{
-	[recorderPopup addItemWithTitle:[[devices objectAtIndex:i] displayName]];
-	
-	deviceStillExsists = ([title isEqualTo:[[devices objectAtIndex:i] displayName]]);
+		[recorderPopup addItemWithTitle:[[devices objectAtIndex:i] displayName]];
 	}
 		
-	if (deviceStillExsists == YES)
-	{
-	[recorderPopup selectItemWithTitle:title];
-	}
+		if ([recorderPopup indexOfItemWithTitle:title] > -1)
+		[recorderPopup selectItemWithTitle:title];
+		
+	[title release];
 	
-[self recorderPopup:self];
+	[self recorderPopup:self];
+}
+
+- (void)saveFrame
+{
+	[[self window] saveFrameUsingName:@"Recorder Info"];
+	[[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 @end
