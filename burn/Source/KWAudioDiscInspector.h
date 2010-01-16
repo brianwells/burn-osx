36946f9888590f441789f60cbfeb0ddfa8379c46
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-16 13:26:29 +0000

    * Removed outlets. * Added an mapping variable to replace the removed outlets.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@148 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWAudioDiscInspector.h
+++ burn/Source/KWAudioDiscInspector.h
@@ -1,26 +1,16 @@
 /* KWAudioDiscInspector */
 
 #import <Cocoa/Cocoa.h>
-#import "audioController.h"
 
 @interface KWAudioDiscInspector : NSObject
 {
-    IBOutlet id arranger;
-    IBOutlet id composer;
-    IBOutlet id discIdent;
     IBOutlet id genreCode;
-    IBOutlet id genreName;
-    IBOutlet id mcn;
-    IBOutlet id mcnCheckBox;
-    IBOutlet id notes;
-    IBOutlet id performer;
-    IBOutlet id privateUse;
-    IBOutlet id songwriter;
-    IBOutlet id timeField;
-    IBOutlet id title;
 	IBOutlet id	myView;
+	IBOutlet id	timeField;
 	
-	audioController *myAudioController;
+	//Variables
+	NSTableView *currentTableView;
+	NSArray *tagMappings;
 }
 - (void)updateView:(id)object;
 - (IBAction)optionsChanged:(id)sender;
