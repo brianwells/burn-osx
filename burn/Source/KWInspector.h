7738b5d98d1f6245d98e5f9c097736bb37e7dece
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-16 12:52:58 +0000

    * Cleaned up and rearranged code.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@130 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWInspector.h
+++ burn/Source/KWInspector.h
@@ -4,30 +4,21 @@
 
 @interface KWInspector : NSWindowController
 {
-    //Data Item
+	//Interface Outlets
+    //Controllers
 	IBOutlet id dataController;
-    IBOutlet id dataView;
-	//Data Disc
 	IBOutlet id dataDiscController;
-    IBOutlet id dataDiscView;
-	//Audio Item
 	IBOutlet id audioController;
-    IBOutlet id audioView;
-	//Audio Disc
 	IBOutlet id audioDiscController;
-    IBOutlet id audioDiscView;
-	//Audio MP3 Item
 	IBOutlet id audioMP3Controller;
-    IBOutlet id audioMP3View;
-	//DVD Item
 	IBOutlet id dvdController;
-    IBOutlet id dvdView;
 	//Empty
 	IBOutlet id emptyView;
 	
 	BOOL firstRun;
 }
 
+//Main Actions
 - (void)beginWindowForType:(NSString *)type withObject:(id)object;
 - (void)updateForType:(NSString *)type withObject:(id)object;
 - (void)saveFrame;
