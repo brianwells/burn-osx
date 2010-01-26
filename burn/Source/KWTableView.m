9ca8af67e283f2d47cdd6f0defb480e4a2b9f4e1
maartenapple <maartenapple@df5e183e-a46d-47f3-acf2-86dc86df8d71>
2010-01-26 11:49:51 +0000

    * Added code to control the 'Burn' button when the table view in de audio and video tab changes.
    git-svn-id: https://burn-osx.svn.sourceforge.net/svnroot/burn-osx/trunk@311 df5e183e-a46d-47f3-acf2-86dc86df8d71

--- burn/Source/KWTableView.m
+++ burn/Source/KWTableView.m
@@ -2,6 +2,12 @@
 
 @implementation KWTableView
 
+- (void)reloadData
+{	
+	[super reloadData];
+	[[NSNotificationCenter defaultCenter] postNotificationName:@"KWChangeBurnStatus" object:[NSNumber numberWithBool:([self numberOfRows] > 0)]];
+}
+
 - (BOOL)becomeFirstResponder 
 {
 	[[NSNotificationCenter defaultCenter] postNotificationName:@"KWListSelected" object:self];
